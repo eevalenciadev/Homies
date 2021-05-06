74fc35cc198bbba88afe266cf94db7db2c2a8507
eevalenciadev <erickvalenciagomez@gmail.com>
2021-05-06 13:36:59 -0500

    Se actualizó service worker

---
+++ public/scripts/sw-utils.js
@@ -0,0 +1,46 @@
+/**
+ * Almacena las respuestas de las URL que serán almacenadas en caché
+ * @param { string } cacheNombre
+ * @param { string[] } urls
+ */
+const agregarCache = async (cacheNombre, urls) => {
+  var cache = await caches.open(cacheNombre);
+  cache.addAll(urls);
+};
+
+/**
+ * Elimina la versiones anteriores del caché que ya no están siendo utilizadas.
+ * @param { string[] } cacheNombres - Nombres de los registros de caché actuales
+ */
+const depurarCache = async (cacheNombres) => {
+  const keys = await caches.keys();
+  const keysAntiguos = keys.filter((key) => !cacheNombres.includes(key));
+  keysAntiguos.forEach((keyAntiguo) => {
+    return caches.delete(keyAntiguo);
+  });
+};
+
+/**
+ *
+ * @param { string } cacheNombre
+ * @param { RequestInfo } request
+ * @param { Response } response
+ */
+const actualizarCache = async (cacheNombre, request, response) => {
+  const cache = await caches.open(cacheNombre);
+  cache.put(request, response);
+  return response;
+};
+
+/**
+ *
+ * @param { string } cacheNombre
+ * @param { RequestInfo } request
+ * @param { Response } response
+ */
+const actualizarCacheDinamico = async (cacheNombre, request, response) => {
+  if (request.url.includes("js/")) {
+    return actualizarCache(cacheNombre, request, response);
+  }
+  return response;
+};
